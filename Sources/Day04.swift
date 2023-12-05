ed95dc8bbd4096d755a372b4c8c946c08736e046
Chad Brady <chadebrady@gmail.com.com>
2023-12-05 14:56:00 -0600

    Day 4 failing on full set

--- Sources/Day04.swift
+++ Sources/Day04.swift
@@ -43,4 +43,45 @@ struct Day04: AdventDay {
       return 1 << ($0.matched().count - 1)
     }.reduce(0, +)
   }
+
+
+  // DOES NOT WORK CORRECTLY WITH FULL SET
+  func part2() -> Any {
+    var lines = data.components(separatedBy: .newlines).filter { !$0.isEmpty }
+    var cards: [Card] = []
+    var copies: [Int] = []
+    var count: Int = 0
+    makeCards(lines: &lines, cards: &cards, copies: &copies, count: &count)
+
+    return count
+  }
+
+  func makeCards(lines: inout [String], cards: inout [Card], copies: inout [Int], count: inout Int) {
+    if lines.isEmpty { return }
+     
+    let line = lines.removeFirst()
+
+    let card = Card(input: line)
+
+    let copyCount = copies.filter { $0 == card.id }.count
+
+    cards.append(card)
+    count += 1
+
+    var wins = card.matched().count
+    var copiedCards: [Int] = []
+    while wins > 0 {
+      let winCopy = card.id + wins
+      copiedCards.append(winCopy)
+      wins -= 1
+    }
+
+    copies.append(contentsOf: copiedCards)
+    count += copiedCards.count * (copyCount + 1)
+    for _ in 0..<copyCount {
+      copies.append(contentsOf: copiedCards)
+    }
+
+    makeCards(lines: &lines, cards: &cards, copies: &copies, count: &count)
+  }
 }
